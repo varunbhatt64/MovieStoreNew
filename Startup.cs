091a9dcf70873fb1a3812e8c4c93fc4c2b58776e
varunbhatt64 <60150618+varunbhatt64@users.noreply.github.com>
2020-03-27 12:44:56 +0530

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieStoreNew.Startup))]
+namespace MovieStoreNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
